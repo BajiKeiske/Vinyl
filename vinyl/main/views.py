6835e72ad3362f6f20a299b3fcb0dc64a2754981
BajiKeiske <kus163166@gmail.com>
2025-05-30 02:07:55 +0300

    new

--- vinyl/main/views.py
+++ vinyl/main/views.py
@@ -20,16 +20,21 @@ def product_detail(request, slug):
                   {'product': product})
 
 def product_list(request, category_slug=None):
+    page = request.GET.get('page', 1)
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    paginator = Paginator(products, 1)
+    current_page = paginator.page(int(page))
     if category_slug:
         category = get_object_or_404(Category,
                                      slug=category_slug)
-        products = products.filter(category=category)
+        paginator = Paginator(products.filter(category=category), 1)
+        current_page = paginator.page(int(page))
     return render(request, 
                   'main/product/list.html',
                   {'category': category,
                    'categories': categories,
-                   'products': products})
+                   'products': current_page, 
+                   'slug_url': category_slug})
     
